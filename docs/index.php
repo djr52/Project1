ff5c67c4dae48c135a4b5fd10889765679b6b8b6
djram <dj.ramirez1221@gmail.com>
2019-10-22 18:45:37 -0400

    Initial commit

---
+++ docs/index.php
@@ -0,0 +1,26 @@
+
+<?php
+//CRUD
+/* Create = POST = INSERT
+* Read = GET = SELECT
+* Update = PUT = UPDATE
+* Delete = DELETE = DELETE
+*/
+
+    $emailAddress = $_POST['email'];
+    if(empty($emailAddress)){
+        echo "Please enter your E-mail";
+    }
+    if(!strpos($emailAddress, '@')){
+
+        echo nl2br("\nPlease type a proper email address.") ;
+    }
+
+    $password = $_POST['password'];
+    if(empty($password) || strlen($password) < 8){
+        echo nl2br("\nPlease enter a valid password. \n(Must be at least 8 characters long.)");
+    }
+
+
+
+
