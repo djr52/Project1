3e19dc1bc59f4981d2ec9be3be228304e4f15708
djram <dj.ramirez1221@gmail.com>
2019-11-24 22:55:22 -0500

    Changed background color

--- docs/display-user-question.php
+++ docs/display-user-question.php
@@ -24,41 +24,40 @@ $_SESSION['email'] = $emailAddress;
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
+    <link rel="stylesheet" href="css/styles.css?v=1.0">
 </head>
-<body>
+<body class="display-question-body">
+<div class="display-question-container">
+    <div>
 
-<div>
+        <h1><?php echo $firstName . " " . $lastName . "'s"; ?> Posted Questions</h1>
 
-    <h1><?php echo $firstName . " " . $lastName . "'s"; ?> Posted Questions</h1>
-
-</div>
-<div>
-    <table class="table">
-        <tr>
-            <th>Email</th>
-            <th>Question Title</th>
-            <th>Question Body</th>
-        </tr>
-        <?php $questions = getQuestionsByEmail($emailAddress); ?>
-        <?php foreach ($questions as $question) : ?>
+    </div>
+    <div>
+        <table class="table">
             <tr>
-                <td><?php echo $question['owneremail'] ?></td>
-                <td><?php echo $question['title'] ?></td>
-                <td><?php echo $question['body'] ?></td>
+                <th>Email</th>
+                <th>Question Title</th>
+                <th>Question Body</th>
             </tr>
+            <?php $questions = getQuestionsByEmail($emailAddress); ?>
+            <?php foreach ($questions as $question) : ?>
+                <tr>
+                    <td><?php echo $question['owneremail'] ?></td>
+                    <td><?php echo $question['title'] ?></td>
+                    <td><?php echo $question['body'] ?></td>
+                </tr>
 
-        <?php endforeach; ?>
-    </table>
+            <?php endforeach;  ?>
+        </table>
 
 
+    </div>
+    <div class="button-display">
+        <a href="new-question-form.html"><input type="button" value="Add New Question" class="add-question-button"></a>
+    </div>
 </div>
 
-    <a href="new-question-form.html"><input type="button" value="Add New Question"></a>
-
-<div>
-
-
-</div>
 
 </body>
 </html>
